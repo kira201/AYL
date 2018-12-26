a0891e2dfd349ecd133f19074a1a1212e850c674
kira201 <806890566@qq.com>
2018-12-26 17:58:57 +0800

    26号上传

--- aylPorject/assets/src/gameBase/Enums.ts
+++ aylPorject/assets/src/gameBase/Enums.ts
@@ -1,6 +1,19 @@
 module gameBase {
-    export enum BirthType {
-        None = 0,
+
+    export enum BirthType { //出身类型
+        None = 0, //谜
+        
         
     }
+
+    export enum BasePropertyType { //人物基础属性类型
+
+    }
+
+    export enum TalentType { //人物天赋属性类型
+
+    }
+
+
+    
 }
